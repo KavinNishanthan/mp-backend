d0cf4b76b82a174b0c303f35ce5817e7a23e4c75
KavinNishanthan <kavinnishanthan2004@gmail.com>
2026-03-28 19:13:56 +0530

    updated server

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,20 @@ import connectDB from './config/db';
 
 dotenv.config();
 
-connectDB();
+const startServer = async () => {
+    try {
+        await connectDB();
 
-const PORT: number = Number(process.env.PORT) || 5000;
+        const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
-});
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+
+    } catch (error) {
+        console.error("Error starting server:", error);
+        process.exit(1);
+    }
+};
+
+startServer();
